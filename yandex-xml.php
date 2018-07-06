025768c22a820e8349225414ca90f1702fdd5f90
Jekeam <suineg@inbox.ru>
2018-07-06 02:50:01 +0000

    test

--- yandex-xml.php
+++ yandex-xml.php
@@ -132,7 +132,7 @@ function search_all($v_keyword, $v_user, $v_key, $v_my_domain, $v_file, $v_curre
             file_put_contents($v_file, $v_current);
         }
         $min = intval(date('i'));
-        $min = (60 - $min);
+        $min = ((60+5) - $min);
         $response = array(  'message' => date("h:i:s", time()) .': "'.$error.'"<br> Повторный запрос через '.$min.' мин.', 
                             'progress' => $p);
         echo json_encode($response);

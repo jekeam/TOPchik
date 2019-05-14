b2c32b8fd6ae0a0f82fbf99ffd7c62679b59e978
suineg <suineg@inbox.ru>
2019-05-14 12:32:12 +0500

    Update top-checker.php

--- top-checker.php
+++ top-checker.php
@@ -1,6 +1,6 @@
 <?php 
 /*
-Plugin Name: TopChick
+Plugin Name: TOPchik
 Plugin URI:  https://vk.com/jekeam
 Description: ТопЧик - Бесплатный плагин для WordPress, 
              который позволяет проверить позиции сайта 
@@ -10,18 +10,18 @@ Author:      Savinykh Aleksand (Савиных Александр Сергеев
 Author URI:  https://vk.com/sun4eese
 License:     GPL2
 
-TopChick is free software: you can redistribute it and/or modify
+TOPchik is free software: you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
 the Free Software Foundation, either version 2 of the License, or
 any later version.
  
-TopChick is distributed in the hope that it will be useful,
+TOPchik is distributed in the hope that it will be useful,
 but WITHOUT ANY WARRANTY; without even the implied warranty of
 MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
 GNU General Public License for more details.
  
 You should have received a copy of the GNU General Public License
-along with TopChick. If not, see https://vk.com/jekeam.
+along with TOPchik. If not, see https://vk.com/jekeam.
 */
 
 //define('TOP_CHECKER_VERSION', '0.1');

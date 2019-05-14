33df05e78d9826c60d0f48043e44bb3512395c68
suineg <suineg@inbox.ru>
2019-05-14 11:54:12 +0500

    Update top-checker.php

--- top-checker.php
+++ top-checker.php
@@ -21,7 +21,7 @@ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
 GNU General Public License for more details.
  
 You should have received a copy of the GNU General Public License
-along with TopChick. If not, see {URI to Plugin License}.
+along with TopChick. If not, see https://vk.com/jekeam.
 */
 
 //define('TOP_CHECKER_VERSION', '0.1');

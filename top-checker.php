4f39a7c2220e52415f8893dc4d92e4b365641d21
suineg <suineg@inbox.ru>
2019-05-14 11:50:10 +0500

    add GPL2

--- top-checker.php
+++ top-checker.php
@@ -1,11 +1,27 @@
-<?php
+<?php 
 /*
-Plugin Name: TopChik
-Plugin URI: https://vk.com/jekeam
-Description: Проверка позиций ключевых слов в поисковой выдаче Яндекса и Google, удобная аналитика.
-Author: Александр Савиных
-Version: Alpha
-Author URI: https://vk.com/sun4eese
+Plugin Name: TopChick
+Plugin URI:  https://vk.com/jekeam
+Description: ТопЧик - Бесплатный плагин для WordPress, 
+             который позволяет проверить позиции сайта 
+	     по запросам в поисковике яндекс в режиме онлайн.	     
+Version:     1.0.0
+Author:      Savinykh Aleksand (Савиных Александр Сергеевич)
+Author URI:  https://vk.com/sun4eese
+License:     GPL2
+
+TopChick is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 2 of the License, or
+any later version.
+ 
+TopChick is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU General Public License for more details.
+ 
+You should have received a copy of the GNU General Public License
+along with TopChick. If not, see {URI to Plugin License}.
 */
 
 //define('TOP_CHECKER_VERSION', '0.1');
